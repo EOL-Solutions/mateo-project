c3a7a11245ada89fc9722cc092c685c8c20f2433
Juan Mateo Albán Méndez <amjuan@unicauca.edu.co>
2023-02-06 19:32:26 -0500

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState, Suspense, useRef, useEffect } from 'react'
+import './App.scss'
+import Header from './components/Header/header'
+import {Section} from './components/Section/section'
+import { Canvas, useFrame } from 'react-three-fiber'
+import { Html, useGLTF } from '@react-three/drei'
+import state from './components/State/state'
+import models from './models.json'
+
+
+
+const Model = ({modelPath, scale=[1, 1, 1]}) => {
+  const gltf = useGLTF(modelPath , true)
+  return <primitive object={gltf.scene} dispose={null} scale={scale}/>
+}
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight position={[10, 10, 5]} intensity={1.5} />
+      <directionalLight position={[0, 10, 0]} intensity={1.5} />
+      <spotLight position={[1000, 0, 0]} intensity={1} />
+    </>
+  )
+}
+
+const HTMLContent = ({children, modelPath, positionY,domContent,scale=[1, 1, 1]}) => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.01));
+
+  return(
+    <Section factor={1.5} offset={1}>
+      <group position={[0, positionY, 0]}>
+        <mesh ref={ref} position={[0, -35, 0]}>
+          <Model modelPath={modelPath} scale={scale}/>
+        </mesh>
+        <Html 
+          portal={domContent}
+          fullscreen>
+             {children}
+        </Html>
+      </group>
+    </Section>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+  const domContent = useRef()
+  const scrollArea = useRef()
+  const onScroll = (e) => (state.top.current = e.target.scrollTop)
+  useEffect(() => void onScroll({ target: scrollArea.current }), [])
+
+  return (
+   <>
+      <Header />
+      <Canvas
+        colorManagement
+        camera={{ position: [0, 0, 120], fov: 90 }}        
+        >
+          <Lights />
+          <Suspense fallback={null}>
+            {
+              models.routes.map((item, index) => (
+                <HTMLContent 
+                  positionY={250*(models.routes.length - index - 3)}
+                  modelPath={`/models/${item.path}/scene.gltf`}
+                  domContent={domContent}
+                  scale={item.scale}>
+                    <div className="container">
+                      <h2 className="title">{item.text}</h2>
+                    </div>
+                </HTMLContent>
+              ))
+            }
+          </Suspense>         
+      </Canvas>
+      <div className="scrollArea" ref={scrollArea} onScroll={onScroll}>
+        <div style={{position:'sticky', top:0} } ref={domContent}>
+
+        </div>
+        <div style={{height:`${state.sections * 100}vh`}}>
+
+        </div>
+      </div>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,31 @@
+import {useState} from "react";
+import styles from './header.module.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <div className={styles['header-inner']}>
+        <div className={styles['logo']}>FURNITURES</div>
+        <nav>
+          <ul>
+            <li>
+              <a href='/'>discover</a>
+            </li>
+            <li>
+              <a href='/'>products</a>
+            </li>
+            <li>
+              <a href='/'>solutions</a>
+            </li>
+            <li>
+              <a href='/'>reach</a>
+            </li>
+            <li className={styles['btn']}>
+              <a href='/'>order</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
